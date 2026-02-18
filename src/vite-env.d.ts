cc7d36761f2266c5cd9e9b705cbb8df2cf428812
Nom et Prénom <contact@go4job.org>
2026-02-18 11:32:54 +0000

    fix: add vite-env module declarations

---
+++ src/vite-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="vite/client" />
+
+declare module "pdfjs-dist/legacy/build/pdf";
+declare module "pdfjs-dist/legacy/build/pdf.worker?url";
+declare module "mammoth/mammoth.browser";
