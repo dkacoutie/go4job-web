916cd32fecc4464433bacbed8d99b99da2e85696
dkacoutie <d.kacoutie@gmail.com>
2026-08-09 03:52:03 +0000

    fix(build): corrige import useToast casse par le split ToastCenter (JR-0129)

--- src/AdminPartnersPage.tsx
+++ src/AdminPartnersPage.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
-import { useToast } from "./components/ToastCenter";
+import { useToast } from "./components/useToast";
 import {
   approvePartnerCommission,
   attachCommissionsToPayout,
